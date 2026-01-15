9bd98fd147fbd6635f63b92f3469db44f2ae5e29
Vaibhavgoel123 <152879577+Vaibhavgoel123@users.noreply.github.com>
2026-01-15 14:57:15 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+class Attention(Layer):
+    def __init__(self, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W = self.add_weight(
+            name="att_weight",
+            shape=(input_shape[-1], 1),
+            initializer='normal'
+        )
+        self.b = self.add_weight(
+            name="att_bias",
+            shape=(input_shape[1], 1),
+            initializer='zeros'
+        )
+        super().build(input_shape)
+
+    def call(self, x):
+        e = tf.keras.backend.tanh(tf.keras.backend.dot(x, self.W) + self.b)
+        a = tf.keras.backend.softmax(e, axis=1)
+        output = x * a
+        return tf.keras.backend.sum(output, axis=1)
